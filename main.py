22e2bcb851af860ae48011342406b95557beeeeb
yuk1tian <ivan.myrhorodskyi@gmail.com>
2025-01-08 19:28:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    return render_template('index.html', button_discord=button_discord,button_python=button_python)
+
+if __name__ == "__main__":
+    app.run(debug=True)
